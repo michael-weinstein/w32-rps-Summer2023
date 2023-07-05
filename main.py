a25276288342e96f92688f3f9fb2818eaec68c98
Michael Weinstein <michael.weinstein@ucla.edu>
2023-07-05 16:27:53 -0700

    game is working

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import rpsSupport
-from rpsSupport import playeroutcome
-from rpsSupport import rules
-from rpsSupport import getPlayerThrows #placeholder
-from rpsSupport import getComputerThrows #placeholder
 
-outcome = playeroutcome.playeroutcome(
-    rules.GameDict,
-    getPlayerThrows.getThrow(rules.GameDict),
-    getComputerThrows.getThrow(rules.GameDict)
-)
 
-if(outcome == "tie"):
-    print("The game is a tie. -_-")
-elif(outcome == "win"):
-    print("The player wins!!! :)")
-else:
-    print("The player losses :(")
+def playGame():
+    gameDict = rpsSupport.rules.rps
+    playerThrow = rpsSupport.getPlayerThrows.getUserInput(gameDict)
+    computerThrow = rpsSupport.getComputerThrow.computerthrow(gameDict)
+    outcome = rpsSupport.playeroutcome.playeroutcome(gameDict, playerThrow, computerThrow)
+    print("Computer chose %s" %computerThrow)
+    if(outcome == "tie"):
+        print("The game is a tie. -_-")
+    elif(outcome == "win"):
+        print("The player wins!!! :)")
+    else:
+        print("The player losses :(")
+
+
+if __name__ == "__main__":
+    playGame()
