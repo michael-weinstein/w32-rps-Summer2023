8f959c47c6b7b081b6769efc96c8ad05e03bca95
Aydin Karatas <alckaratas18@gmail.com>
2023-07-05 15:33:44 -0700

    print out options; take in/return user input

--- rpsSupport/getThrows.py
+++ rpsSupport/getThrows.py
@@ -1,5 +1,8 @@
-def getUserInput(gamedict: dict):
+def getUserInput(gamedict: dict) -> str:
     inputs= gamedict.keys()
     print("Throw options:")
     listofnumbers=range(1,len(inputs))
-    
+    for i in listofnumbers:
+        print(f"{i}: {inputs[i-1]}")
+    userInput=input("Choose your throw:")
+    return inputs[userInput-1]
