81b987cba489adcb3cb283d892a95d2ead3812af
Michael Weinstein <michael.weinstein@ucla.edu>
2023-07-05 20:01:27 -0700

    removed extra files

--- getcompthrow.py
+++
@@ -1,9 +0,0 @@
-import random
-import typing
-def computerthrow(gamedict:typing.Dict[str, typing.List[str]]) -> computerthrow:str
-    choice_list = ['rock','paper','scissors']
-    computerchoice = random.choice(choice_list)
-
-    return computerchoice
-
-
